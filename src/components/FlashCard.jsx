09dd0d48997a84f98d6bdfc2ffb18fa29118b25d
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2025-01-08 00:25:36 +0530

    fix

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -19,22 +19,34 @@ const FlashCard = () => {
     const handleMarkAsLearned = (id) => {
         dispatch(markAsLearned(id))
     }
+
     if (words.length === 0) return <p>Add Word to view flashCard</p>
+
     const currentWord = words[index];
 
     if (!currentWord) return <p>No Valid word available</p>
+
     return (
         <div className={`flex flex-col gap-10 items-center ${darkMode ? "bg-gray-800" : "bg-white "} justify-center w-full h-full `}>
 
             <h2 className='text-center md:text-4xl  mt-6 text-2xl'>FlashCard</h2>
 
-            <div onClick={handleFlip} className={`lg:h-72 shadow-2xl lg:w-72 w-60 h-60 flex items-center justify-center  rounded-3xl ${darkMode ? "bg-gray-800 text-white border-white border-2" : "bg-white text-black border-black border-2"} `}>
-                <h2 className='lg:text-2xl text-center text-xl'>{flip ? "Translation" : "Word"}: <br />{flip ? currentWord.translation : currentWord.word}</h2>
+            <div onClick={handleFlip} className={`flip-card lg:h-72 lg:w-72 w-60 h-60 flex items-center justify-center  rounded-3xl ${darkMode ? "bg-gray-800 text-white " : "bg-white text-black "}`}>
+                <div className={`flip-card-inner ${flip ? 'flipped' : ''}`}>
+                    <div className="flip-card-front shadow-2xl">
+                        <h2 className='lg:text-2xl text-center text-xl'>
+                            Word: <br />{currentWord.word}</h2>
+                    </div>
+                    <div className="flip-card-back shadow-2xl">
+                        <h2 className='lg:text-2xl text-center text-xl'>
+                            Translation: <br />{currentWord.translation}</h2>
+                    </div>
+                </div>
             </div>
 
             <div className='flex items-center justify-center gap-5'>
-                <button onClick={handleNext} className='lg:text-xl text-lg px-6 py-2 rounded-md outline-none bg-blue-500 text-white  font-semibold'>Next</button>
-                <button onClick={() => handleMarkAsLearned(currentWord.id)} className='md:text-xl text-lg px-6 py-2 rounded-md outline-none  bg-green-500 text-white font-semibold'>{currentWord.learned ? "Marked" : "Mark as Learned"}</button>
+                <button onClick={handleNext} className='lg:text-xl text-lg px-6 py-2 rounded-md outline-none bg-blue-500 text-white font-semibold'>Next</button>
+                <button onClick={() => handleMarkAsLearned(currentWord.id)} className='md:text-xl text-lg px-6 py-2 rounded-md outline-none bg-green-500 text-white font-semibold'>{currentWord.learned ? "Marked" : "Mark as Learned"}</button>
             </div>
 
         </div>
